fbc327ef89d94c6d0e1e16f7cb70205b97dbc17c
Emanuelly <emanuellygomes.manu2004@gmail.com>
2025-03-22 19:37:36 -0300

    fix exercicio01

--- atividades-avaliativas/Exercicio01-com-threads.py
+++ atividades-avaliativas/Exercicio01-com-threads.py
@@ -39,20 +39,41 @@ def quicksort_paralelizada(arr):
 
     return leftList + [pivot] + rightList
 
+    # Observação: já que as threads ocorrem em listas separadas (left e right) onde os elementos não serão
+    # compartilhados e terminam com o join() antes de entrar novamente na recursividade, descartamos o risco de 
+    # ocorrência de DeadLock e portanto também a necessidade de usar threading.lock().  
+
 # Função para gerar números aleatórios
-def gerar_numeros_aleatorios(n=500, min_val=1, max_val=200):
+def gerar_numeros_aleatorios(n=100, min_val=1, max_val=200):
     return [random.randint(min_val, max_val) for _ in range(n)]
 
 # Função principal para testar o QuickSort
 if __name__ == "__main__":
     numeros = gerar_numeros_aleatorios() 
-    print(f"\nPrimeiros 10 números antes da ordenação: {numeros[:10]}\n") 
+    print(f"\nPrimeiros 10 números antes da ordenação: {numeros[:10]}") 
     # Adicionamos '[:10]' para de fato mostrar apenas os 10 primeiros numeros e facilitar a visualização no terminal 
 
+    print("Aguarde a execução...\n")
+
+    # QuickSort normal
+    inicio = time.time()
+    numeros_ordenados = quicksort(numeros)
+    fim = time.time()
+    tempoExecucao = fim - inicio
+
+    # QuickSort com threads
     inicio = time.time()
     numeros_ordenados = quicksort_paralelizada(numeros)
     fim = time.time()
-    tempoExecucao = fim - inicio
+    tempoExecucaoParalela = fim - inicio
    
-    print(f"Primeiros 10 números após a ordenação: {numeros_ordenados[:10]}\n")
-    print(f"Tempo de execução: {tempoExecucao:.5f}\n")    
+    print(f"Primeiros 10 números após a ordenação: {numeros_ordenados[:10]}")
+
+    print(f"Tempo de execução sem paralelização: {tempoExecucao:.4f}") 
+    print(f"Tempo de execução com paralelização: {tempoExecucaoParalela:.4f}\n")    
+
+    print("Resultado do teste: ")
+    if tempoExecucaoParalela < tempoExecucao:
+        print(f"A execução paralela foi {(tempoExecucao - tempoExecucaoParalela):.4f} segundos mais rápida.\n")
+    else:
+        print(f"A execução sem threads foi {(tempoExecucaoParalela - tempoExecucao):.4f} segundos mais rápida.\n")
